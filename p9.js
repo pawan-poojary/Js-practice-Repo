7da05080ebaff6a7bce657b266f305fe16707870
pawan-poojary <pawan@accelerlab.in>
2026-03-24 12:18:43 +0530

    made changes in objects

--- p9.js
+++ p9.js
@@ -3,9 +3,11 @@
 let user={
     name:"Rahul",
     age:21,
-    lang:"python"
+    lang:"python",
+    address:"Moodbidri"
 };
 
 console.log(user.name);
 console.log(user.medium);
 console.log(user.age);
+console.log(user.address)
