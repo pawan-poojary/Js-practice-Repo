3d365864426d77029cbbdc8c0bde44f95e4c00ef
pawan-poojary <pawan@accelerlab.in>
2026-03-24 12:27:12 +0530

    test

--- p9.js
+++ p9.js
@@ -7,7 +7,7 @@ let user={
     address:"Moodbidri"
 };
 
-console.log(user.name);
-console.log(user.medium);
-console.log(user.age);
-console.log(user.address)
+// console.log(user.name);
+// console.log(user.medium);
+// console.log(user.age);
+// console.log(user.address)
